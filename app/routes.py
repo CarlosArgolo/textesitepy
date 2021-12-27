a44a46e0a0e45b98dc7dfc8638ba6fefe3596ebd
Carlos <carlos.argolo@hotmail.com>
2021-12-27 19:36:11 -0300

    ccccccccccccccttttttrr

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,14 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    titulo = 'Contatos'
+    titulo = 'Home'
     nome = "Carlos"
-    return render_template('index.html',nome=nome, titulo=titulo)
+    arqCss ='principal.css'
+    return render_template('index.html',nome=nome, titulo=titulo, arqCss=arqCss)
 
 @app.route('/contatos')
 def contatos():
     titulo = 'Contatos'
     ms = "Meu Contato com essa ai"
-    return render_template('contatos.html',ms=ms, titulo=titulo)
+    arqCss ='principal.css'
+    return render_template('contatos.html',ms=ms, titulo=titulo, arqCss=arqCss)
