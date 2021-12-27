d70f445ba0b4aac5d3f7ce73a48e2b9f9be5949a
Carlos <carlos.argolo@hotmail.com>
2021-12-27 16:55:19 -0300

    llllllllll

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,8 @@ from flask import render_template
 def index():
     nome = "Carlos"
     return render_template('index.html',nome=nome)
+
+@app.route('/contatos')
+def index():
+    ms = "Meu Contato com essa ai"
+    return render_template('contatos.html',ms=ms)
